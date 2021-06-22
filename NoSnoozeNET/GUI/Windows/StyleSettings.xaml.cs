5f07407900ea9cafe4045dc142699c6a8f3880b7
Cruxial0 <benjamin@theones.dk>
2021-06-23 01:17:03 +0200

    Added comments

--- NoSnoozeNET/GUI/Windows/StyleSettings.xaml.cs
+++ NoSnoozeNET/GUI/Windows/StyleSettings.xaml.cs
@@ -24,14 +24,20 @@ namespace NoSnoozeNET.GUI.Windows
             InitializeComponent();
             CreatePreview();
 
+            //Sets ItemSource of ComboBox to loaded UserThemes.
             cmbThemes.ItemsSource = MainWindow.GlobalConfig.ThemeConfig.Keys;
 
+            //Checks or Unchecks CheckBox according to UsingAdvanced Property.
             UseAdvanced.IsChecked = MainWindow.GlobalConfig.BrushConfig.MainBrush.UsingAdvanced;
 
+            //Sets the selected theme as SelectedItem.
             cmbThemes.SelectedItem = MainWindow.GlobalConfig.SelectedTheme;
 
         }
 
+        /// <summary>
+        /// Sets color of all color pickers.
+        /// </summary>
         public void SetColorPickers()
         {
             clrPrimary.SelectedColor = ((SolidColorBrush)Application.Current.Resources["LabelBrush"]).Color;
@@ -47,12 +53,13 @@ namespace NoSnoozeNET.GUI.Windows
 
         private void CreatePreview()
         {
+            //Make new list of AlarmItem.
             previewItem = new List<AlarmItem>();
 
+            //Set source to newly generated list.
             ControlContainer.ItemsSource = previewItem;
 
-            Size half = new Size(220, 52.5);
-
+            //Declare and populate new AlarmItem.
             AlarmItem a = new AlarmItem()
             {
                 AlarmName = "Test Alarm",
@@ -60,10 +67,15 @@ namespace NoSnoozeNET.GUI.Windows
                 TimeToRing = "Rings at 8PM (in 7h)",
             };
 
+            //Scale AlarmItem by 0.75x.
             a.LayoutTransform = new ScaleTransform(0.75, 0.75);
 
+            //Add AlarmItem to list.
             previewItem.Add(a);
         }
+
+        //Region to group the CTRL+C/CTRL+V spam
+        #region ControlSpam
 
         private void ClrPrimary_OnSelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
         {
@@ -119,11 +131,15 @@ namespace NoSnoozeNET.GUI.Windows
             MainWindow.GlobalConfig.BrushConfig.ApplyBrushConfig();
         }
 
+        #endregion
+
         private void UseAdvanced_OnChecked(object sender, RoutedEventArgs e)
         {
+            //Set UsingAdvanced property to check state.
             if (UseAdvanced.IsChecked != null)
                 MainWindow.GlobalConfig.BrushConfig.MainBrush.UsingAdvanced = UseAdvanced.IsChecked.Value;
 
+            //Show advanced Settings
             WindowAdvanced.Visibility = Visibility.Visible;
             WindowAdvanced.IsEnabled = true;
 
@@ -131,39 +147,51 @@ namespace NoSnoozeNET.GUI.Windows
 
         private void UseAdvanced_OnUnchecked(object sender, RoutedEventArgs e)
         {
+            //Set UsingAdvanced property to check state.
             if (UseAdvanced.IsChecked != null)
                 MainWindow.GlobalConfig.BrushConfig.MainBrush.UsingAdvanced = UseAdvanced.IsChecked.Value;
 
+            //Hide advanced Settings
             WindowAdvanced.Visibility = Visibility.Hidden;
             WindowAdvanced.IsEnabled = false;
         }
 
         private async void CmbThemes_OnDropDownClosed(object sender, EventArgs e)
         {
+            //Check that an item is selected.
             if (cmbThemes.SelectedIndex != -1)
             {
+                //Build ThemeConfig.
                 MainWindow.GlobalConfig.BuildThemeConfig();
+                //Set selected theme to ComboBox value.
                 MainWindow.GlobalConfig.SelectedTheme = (string)cmbThemes.SelectedValue;
+                //Get value from Dictionary.
                 MainWindow.GlobalConfig.ThemeConfig.TryGetValue((string)cmbThemes.SelectedValue, out var brushConfig);
+                //Apply the theme.
                 ThemePresetMethods.ApplyTheme(brushConfig);
 
+                //Color preview item.
                 await Dispatcher.InvokeAsync(() => previewItem.First().ColorStopwatch(MainWindow.GlobalConfig.BrushConfig.AlarmItemBrush.StopwatchBrush), DispatcherPriority.Render);
                 await Dispatcher.InvokeAsync(() => previewItem.First().ColorOptions(MainWindow.GlobalConfig.BrushConfig.AlarmItemBrush.OptionsBrush), DispatcherPriority.Render);
 
+                //Refresh window
                 WindowExt.Refresh(this);
 
+                //Set color pickers accordingly.
                 SetColorPickers();
             }
         }
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
+            //Create new instance of SaveTheme, then show window.
             SaveTheme st = new SaveTheme();
             st.Show();
         }
 
         private void BtnOpenFolder_OnClick(object sender, RoutedEventArgs e)
         {
+            //Open Theme Directory in File Explorer.
             Process.Start("explorer.exe", ThemeHandler.ThemeDirectory);
         }
     }
